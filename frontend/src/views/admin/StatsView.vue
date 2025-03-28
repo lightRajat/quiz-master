68835f99039f15e0a242257ef5db3b5acc44d9b8
Rajat <24f1002405@ds.study.iitm.ac.in>
2025-03-29 01:02:51 +0530

    Added a download option for charts on admin page

--- frontend/src/views/admin/StatsView.vue
+++ frontend/src/views/admin/StatsView.vue
@@ -7,19 +7,15 @@ import { api } from "@/utils/auth";
 
 // chartjs config
 ChartJS.register(Title, Tooltip, ArcElement, ChartDataLabels, BarElement, CategoryScale, LinearScale, Legend);
-
 ChartJS.defaults.font.family = "'Poppins', sans-serif";
 ChartJS.defaults.font.size = 16;
 ChartJS.defaults.font.weight = "bold";
 ChartJS.defaults.color = "#333";
-
 ChartJS.defaults.plugins.title.font.size = 22;
 ChartJS.defaults.plugins.title.font.weight = "bold";
 ChartJS.defaults.plugins.title.color = "#111";
-
-
 ChartJS.defaults.plugins.datalabels = {
-  color: "#fff",
+  color: "#333",
   font: {
     size: 16,
     weight: "bold",
@@ -63,6 +59,7 @@ const scopeDist = ref({
         },
     },
 });
+const scopeDistChart = ref(null);
 
 const dayAttemptedDist = ref({
     visible: false,
@@ -76,6 +73,7 @@ const dayAttemptedDist = ref({
     },
     options: {
         responsive: true,
+        maintainAspectRatio: true,
         plugins: {
             legend: {
                 display: true,
@@ -97,10 +95,38 @@ const dayAttemptedDist = ref({
         },
     },
 });
+const dayAttemptedDistChart = ref(null);
 
 const getPercentage = (value, total) => {
     return Math.round(value / total * 100);
 };
+
+const downloadChart = (type) => {
+    let chartElem;
+    if (type == 'pie') {
+        chartElem = scopeDistChart.value;
+    } else {
+        chartElem = dayAttemptedDistChart.value;
+    }
+    if (!chartElem?.chart) return;
+
+    const canvas = chartElem.chart.canvas;
+
+    // make white bg
+    const newCanvas = document.createElement("canvas");
+    newCanvas.width = canvas.width;
+    newCanvas.height = canvas.height;
+
+    const ctx = newCanvas.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(canvas, 0, 0);
+
+    const anchorElem = document.createElement("a");
+    anchorElem.href = newCanvas.toDataURL("image/jpeg");
+    anchorElem.download = type == 'pie' ? 'quizzesAcrossScopeChart.jpg' : 'quizzesAcrossDaysChart.jpg';
+    anchorElem.click();
+}
 
 onMounted(async () => {
     try {
@@ -141,16 +167,36 @@ onMounted(async () => {
 
 <template>
     <main class="p-5">
-        <div class="d-flex align-items-center">
+        <div class="d-flex align-items-end">
 
+            <!-- pie chart -->
             <div class="chart-container">
-                <Pie :data="scopeDist.data" :options="scopeDist.options"
-                v-if="scopeDist.visible" />
+
+                <div>
+                    <Pie :data="scopeDist.data" :options="scopeDist.options"
+                    v-if="scopeDist.visible" ref="scopeDistChart" />
+                </div>
+
+                <button class="btn btn-primary mt-4" @click="downloadChart('pie')">
+                    <i class="bi bi-download me-1"></i>
+                    Download Chart as PNG
+                </button>
+
             </div>
 
+            <!-- bar chart -->
             <div class="chart-container">
-                <Bar :data="dayAttemptedDist.data" :options="dayAttemptedDist.options"
-                v-if="dayAttemptedDist.visible" />
+
+                <div>
+                    <Bar :data="dayAttemptedDist.data" :options="dayAttemptedDist.options"
+                    v-if="dayAttemptedDist.visible" ref="dayAttemptedDistChart" />
+                </div>
+
+                <button class="btn btn-primary mt-4" @click="downloadChart('bar')">
+                    <i class="bi bi-download me-1"></i>
+                    Download Chart as PNG
+                </button>
+
             </div>
 
         </div>
@@ -161,5 +207,6 @@ onMounted(async () => {
 .chart-container {
     width: 50%;
     padding: 16px;
+    text-align: center;
 }
 </style>
